e107746c362d35efe12e67fa72b5f40abb15af41
Ольга Гаджиева <ventzios@yanndex.ru>
2023-05-01 17:53:10 +0500

    develop

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from reviews.models import CustomUser, Category, Genre, Title
+from reviews.models import CustomUser, Category, Genre, Title, Review, Comment
 # from reviews.models import (Category, Comment, Genre, Title,
 #                            Review, CustomUser)
 
@@ -23,7 +23,17 @@ class GenreTitleAdmin(admin.ModelAdmin):
     list_display = ("pk", "name", "year", "description", "category")
 
 
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("pk", "text", "title", "author", "score", "pub_date")
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("pk", "text", "review", "author", "pub_date")
+
+
 admin.site.register(CustomUser, UserAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Genre, GenreAdmin)
 admin.site.register(Title, GenreTitleAdmin)
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Comment, CommentAdmin)
