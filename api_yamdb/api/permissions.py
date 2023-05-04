af7f05c394bf8c9a91f9b285a77d839625a2dd21
Михаил Матросов <matrosovmn@gmail.comd:/VS Code/Yandex Practicum/  /venv/Scripts/python.exe>
2023-05-04 05:14:22 +0500

    update permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -4,14 +4,21 @@ from rest_framework.permissions import (
 
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.is_admin
+        return (
+            request.user.is_authenticated
+            and request.user.is_admin_or_super_user
+        )
 
 
 class IsAdminOrModeratorOrAuthorOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return (request.method in SAFE_METHODS
+                or request.user.is_authenticated)
+
     def has_object_permission(self, request, view, obj):
         return (
             request.method in SAFE_METHODS
-            or request.user.is_admin
+            or request.user.is_admin_or_super_user
             or request.user.is_moderator
             or request.user == obj.author
         )
@@ -23,6 +30,6 @@ class IsAdminUserOrReadOnly(BasePermission):
             request.method in SAFE_METHODS
             or (
                 request.user.is_authenticated
-                and request.user.is_admin
+                and request.user.is_admin_or_super_user
             )
         )
